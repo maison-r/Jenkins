a67d49a9c62abb2a45f44ff6fa981efba895d5a7
maison-r <74816486+maison-r@users.noreply.github.com>
2024-08-28 19:03:01 +1000

    upload 2

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,21 @@
+Jenkinsfile
+Jenkinsfile
+pipeline {
+    agent any
+    stages {
+        stage('Build') {
+            steps {
+                echo "Building the code using Maven (Build automation tool)."
+                echo 'Tool: Maven'
+            }
+            post {
+                success{
+                    mail to: "m.rovacsek@hotmail.com",
+                    subject: "build status",
+                    body: "build was successful"
+                }
+            }
+        }
+    }
+}
+         
